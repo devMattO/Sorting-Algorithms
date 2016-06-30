c0f0839254576ce66d378cdd10f629580cff186d
Mattdre <olsen_matthew@yahoo.com>
2016-06-29 15:16:50 -1000

    initial commit

---
+++ js/lib/bubble_sort.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var currentNum;
+var nextNum;
+
+var bubbleSort = function( input ){
+  var flag = true;
+  var counter = 0;
+  while(flag){
+    flag = false;
+    for (var i = 0; i < input.length; i++) {
+      currentNum = input[i];
+      nextNum = input[i + 1];
+      if( nextNum < currentNum ){
+        input.splice(i, 2, nextNum, currentNum);
+        flag = true;
+      }
+    }
+  }
+  return input;
+};
---
+++ js/test/bubble_sort.spec.js
@@ -0,0 +1,12 @@
+describe('Bubble Sort Alg', function(){
+
+  it('should be a function', function(){
+    expect(bubbleSort).to.be.a('function');
+  });
+
+  it('should take an input Array and return sorted array', function(){
+    var arr = [7,2,8,88,49,29,5,4,9,6,3,1];
+    expect(bubbleSort(arr)).to.deep.equal([1,2,3,4,5,6,7,8,9,29,49,88]);
+  });
+
+});
