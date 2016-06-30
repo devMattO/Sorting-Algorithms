b1961b82b0d3a29fdb2965c9e3654fd46f16cb85
Mattdre <olsen_matthew@yahoo.com>
2016-06-29 15:32:22 -1000

    Finished quick sort, woooooohooooo

--- js/test/quick_sort.spec.js
+++ js/test/quick_sort.spec.js
@@ -4,10 +4,6 @@ describe('Quick Sort Alg', function(){
     expect(quickSort).to.be.a('function');
   });
 
-  it('should be a function', function(){
-    expect(parition).to.be.a('function');
-  });
-
   it('should take an input Array and return sorted array', function(){
     var arr = [7,2,8,88,49,29,5,4,9,6,3,1];
     expect(quickSort(arr)).to.deep.equal([1,2,3,4,5,6,7,8,9,29,49,88]);
